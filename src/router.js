278037f6369023684262626829bc4ced1480ba2b
wwyy <15268131701@163.com>
2020-12-03 13:57:32 +0800

    router.js路由封装

--- src/router.js
+++ src/router.js
@@ -1,26 +1,8 @@
 import Vue from "vue"
 import Router from "vue-router"
 
-import layout from './views/layout.vue';
+import routes from './common/config/router.js';
 
 Vue.use(Router)
 
-export default new Router({
-	routes: [
-		{
-			path: '/',
-			name: 'layout',
-			component: layout,
-			children: [{
-				path: '/index',
-				name: 'index',
-				component: () => import('./views/index/index.vue')
-			}]
-		},
-		{
-			path: '/login',
-			name: 'login',
-			component: () => import('./views/login/index.vue')
-		}
-	]
-})
+export default new Router({ routes })
